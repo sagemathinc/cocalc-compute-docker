946fdffc2f185a716932ea46d6863ad8aa4f64f3
William Stein <wstein@sagemath.com>
2023-08-27 17:04:30 -0700

    fix x86 dockerhub script

--- x86_64/update-the-build-stage-1.sh
+++ x86_64/update-the-build-stage-1.sh
@@ -2,5 +2,5 @@
 set -v
 docker stop cocalc-compute-test
 docker rm cocalc-compute-test
-docker push  sagemathinc/cocalc-compute-aarch64:latest
-docker push  sagemathinc/cocalc-compute-aarch64:`cat current_commit`
+docker push  sagemathinc/cocalc-compute:latest
+docker push  sagemathinc/cocalc-compute:`cat current_commit`
