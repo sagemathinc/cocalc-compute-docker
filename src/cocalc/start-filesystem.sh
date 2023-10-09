6af8a9d218a5c76cb90f15cade441f514c4ec08f
William Stein <wstein@sagemath.com>
2023-10-09 03:20:59 +0000

    make path to nodejs more robust

--- src/cocalc/start-filesystem.sh
+++ src/cocalc/start-filesystem.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
-source /cocalc/nvm/nvm.sh
+#source /cocalc/nvm/nvm.sh
+
+export PATH=/cocalc/nvm/versions/node/v18.17.1/bin/:$PATH
 cd /cocalc/src/compute/compute
 node ./start-filesystem.js
--- src/cocalc/start-manager.sh
+++ src/cocalc/start-manager.sh
@@ -2,6 +2,10 @@
 
 set -ev
 
-source /cocalc/nvm/nvm.sh
+# This does NOT work if there is already an nvm, and sometimes there is, e.g., for the tensorflow image.
+# source /cocalc/nvm/nvm.sh
+
+# This always works:
+export PATH=/cocalc/nvm/versions/node/v18.17.1/bin/:$PATH
 cd /cocalc/src/compute/compute
 node ./start-manager.js
