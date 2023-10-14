9587435aaea9d9e69cfa0f18d328cdea984dface
William Stein <wstein@sagemath.com>
2023-10-14 18:34:37 +0000

    read api key, projct_id and compute server id from filesystem instead of docker command line
    - this will make it possible to very frequently rotate api key
    - also, can move a compute server between projects and *keep the data*

--- src/cocalc/start-filesystem.sh
+++ src/cocalc/start-filesystem.sh
@@ -2,8 +2,11 @@
 
 set -ev
 
-#source /cocalc/nvm/nvm.sh
-
 export PATH=/cocalc/nvm/versions/node/v18.17.1/bin/:$PATH
+
+export API_KEY=`cat /cocalc/conf/api_key`
+export PROJECT_ID=`cat /cocalc/conf/project_id`
+export COMPUTE_SERVER_ID=`cat /cocalc/conf/compute_server_id`
+
 cd /cocalc/src/compute/compute
 node ./start-filesystem.js
--- src/cocalc/start-manager.sh
+++ src/cocalc/start-manager.sh
@@ -7,5 +7,10 @@ set -ev
 
 # This always works:
 export PATH=/cocalc/nvm/versions/node/v18.17.1/bin/:$PATH
+
+export API_KEY=`cat /cocalc/conf/api_key`
+export PROJECT_ID=`cat /cocalc/conf/project_id`
+export COMPUTE_SERVER_ID=`cat /cocalc/conf/compute_server_id`
+
 cd /cocalc/src/compute/compute
 node ./start-manager.js
