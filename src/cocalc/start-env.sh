c61e5c1864225c8d696bf63975d1f4b5e88cb859
William Stein <wstein@sagemath.com>
2023-10-18 15:40:54 +0000

    setting of hostname; also easy enabling/disabling of DEBUG for compute server

--- src/cocalc/start-env.sh
+++ src/cocalc/start-env.sh
@@ -4,8 +4,12 @@ set -ev
 
 # Setup env vars about project we will connect to.
 
+export LOGS='/tmp/logs'
 export API_KEY=`cat /cocalc/conf/api_key`
 export API_SERVER=`cat /cocalc/conf/api_server`
 export PROJECT_ID=`cat /cocalc/conf/project_id`
 export COMPUTE_SERVER_ID=`cat /cocalc/conf/compute_server_id`
-export HOSTNAME=`cat /cocalc/conf/hostname`
+export DEBUG=$(test -f /cocalc/conf/debug && cat /cocalc/conf/debug || echo "")
+
+sudo hostname `cat /cocalc/conf/hostname`
+echo '127.0.0.1 `cat /cocalc/conf/hostname`' | sudo tee -a /etc/hosts
