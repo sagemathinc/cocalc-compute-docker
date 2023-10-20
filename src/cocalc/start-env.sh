4c9ab5450135a9a5a0c6cc88bf8aa7e96cacce45
William Stein <wstein@sagemath.com>
2023-10-20 15:20:19 +0000

    figured out how to filesystem in one container with two processes without deadlock

--- src/cocalc/start-env.sh
+++ src/cocalc/start-env.sh
@@ -4,14 +4,15 @@ set -ev
 
 # Setup env vars about project we will connect to.
 
+unset COCALC_PROJECT_ID
 export LOGS='/tmp/logs'
 export API_KEY=`cat /cocalc/conf/api_key`
 export API_SERVER=`cat /cocalc/conf/api_server`
 export PROJECT_ID=`cat /cocalc/conf/project_id`
 export COMPUTE_SERVER_ID=`cat /cocalc/conf/compute_server_id`
 export DEBUG=$(test -f /cocalc/conf/debug && cat /cocalc/conf/debug || echo "")
-# export UNIONFS_UPPER=/home/unionfs/upper
-# export UNIONFS_LOWER=/home/unionfs/lower
+export UNIONFS_UPPER=/home/unionfs/upper
+export UNIONFS_LOWER=/home/unionfs/lower
 export PROJECT_HOME=/home/user
 
 sudo hostname `cat /cocalc/conf/hostname`
--- src/cocalc/start-filesystem.sh
+++ src/cocalc/start-filesystem.sh
@@ -5,6 +5,29 @@ set -ev
 export PATH=/cocalc/nvm/versions/node/v18.17.1/bin/:$PATH
 
 source /cocalc/start-env.sh
-
 cd /cocalc/src/compute/compute
-node ./start-filesystem.js
+
+if [[ -z "$UNIONFS_UPPER" ]]; then
+    # unionfs not configured at all.
+    node ./start-filesystem.js
+    exit $?
+fi
+
+# We are using unionfs.  We can't run the websocketetfs FUSE filesystem
+# in the same process as the unionfs cache that makes a websocket connection
+# to the project, since this leads to deadlock, at least in docker.
+# So we start start-filesystem.js twice, once with unionfs (which doesn't
+# mount websocketfs),  and the second time with *just* websocketfs.
+
+mkdir -p /tmp/logs
+
+# process that runs unionfs
+exec node ./start-filesystem.js 2>&1 > /tmp/logs/unionfs.log &
+
+# process that runs just websocketfs to mount /home/unionfs/lower
+export PROJECT_HOME=$UNIONFS_LOWER
+unset UNIONFS_UPPER
+unset UNIONFS_LOWER
+exec node ./start-filesystem.js  2>&1 > /tmp/logs/websocketfs.log &
+
+tail -F /tmp/logs/unionfs.log  /tmp/logs/websocketfs.log
