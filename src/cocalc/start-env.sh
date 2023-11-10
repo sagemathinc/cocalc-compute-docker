c4c36b53f97d5ee96d464dca5470d0a866b88715
William Stein <wstein@sagemath.com>
2023-11-10 07:26:52 +0000

    some reorg of startup

--- src/cocalc/start-env.sh
+++ src/cocalc/start-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ev
+set -e
 
 # Setup env vars about project we will connect to.
 
@@ -16,17 +16,9 @@ export UNIONFS_UPPER=/home/unionfs/upper
 export UNIONFS_LOWER=/home/unionfs/lower
 export PROJECT_HOME=/home/user
 
-# This could be anywhere.  Setting it enabled read tracking at the websocketfs level,
-# which is then used by our caching level using unionfs to automatically keep
-# files locally when they are read, so future reads are fast and free.
-mkdir -p /home/unionfs/upper/.compute-server/
-export READ_TRACKING_PATH=/home/unionfs/upper/.compute-server/read-tracking
-echo "" > $READ_TRACKING_PATH
-
 sudo hostname `cat /cocalc/conf/hostname`
 echo "127.0.0.1 `cat /cocalc/conf/hostname`" | sudo tee -a /etc/hosts
 
-# Make the NEWEST version of nodejs available.
-source /cocalc/nvm/nvm.sh
-nvm use node
+# Make the cocalc version of nodejs available.
+NVM_DIR=/cocalc/nvm source /cocalc/nvm/nvm.sh
 
