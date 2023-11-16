3953482c7a45bace59fbe99ff9524c414d93e1f5
William Stein <wstein@sagemath.com>
2023-11-16 04:04:11 +0000

    use env vars more

--- src/cocalc/start-filesystem.sh
+++ src/cocalc/start-filesystem.sh
@@ -5,15 +5,22 @@ set -ev
 source /cocalc/start-env.sh
 
 # This directory could be anywhere.
-mkdir -p /home/unionfs/upper/.compute-server/
+mkdir -p $UNIONFS_UPPER/.compute-server/
 
 # Setting it enabled read tracking at the websocketfs level,
 # which is then used by our caching level using unionfs to automatically keep
 # files locally when they are read, so future reads are fast and free.
-export READ_TRACKING_PATH=/home/unionfs/upper/.compute-server/read-tracking
+export READ_TRACKING_PATH=$UNIONFS_UPPER/.compute-server/read-tracking
 echo "" > $READ_TRACKING_PATH
 
+export METADATA_FILE=$UNIONFS_UPPER/.compute-server/meta/meta.lz4
+
 cd /cocalc/src/compute/compute
+
+# Make sure filesystems aren't mounted
+
+umount $UNIONFS_LOWER  2>/dev/null || true
+umount $PROJECT_HOME   2>/dev/null || true
 
 if [[ -z "$UNIONFS_UPPER" ]]; then
     # unionfs not configured at all.
@@ -32,7 +39,7 @@ mkdir -p /tmp/logs
 # process that runs unionfs
 exec node ./start-filesystem.js 2>&1 > /tmp/logs/unionfs.log &
 
-# process that runs just websocketfs to mount /home/unionfs/lower
+# process that runs just websocketfs to mount $UNIONFS_LOWER
 export PROJECT_HOME=$UNIONFS_LOWER
 unset UNIONFS_UPPER
 unset UNIONFS_LOWER
