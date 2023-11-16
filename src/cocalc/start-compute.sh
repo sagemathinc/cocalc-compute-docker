e5d19522bf4df3bc74a81b3014bd23f701db404f
William Stein <wstein@sagemath.com>
2023-11-16 22:36:07 +0000

    more cleanup

--- src/cocalc/start-compute.sh
+++ src/cocalc/start-compute.sh
@@ -7,5 +7,8 @@ set -ev
 
 source /cocalc/start-env.sh
 
+sudo hostname `cat /cocalc/conf/hostname`
+echo "127.0.0.1 `cat /cocalc/conf/hostname`" | sudo tee -a /etc/hosts
+
 cd /cocalc/src/compute/compute
 node ./start-compute.js
