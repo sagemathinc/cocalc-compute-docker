83460b1ae44a026986c22c9076f703a4befdd971
Harald Schilly <harald.schilly@gmail.com>
2023-11-16 18:49:50 +0100

    tweaking miniforge

--- src/miniforge/miniforge.sh
+++ src/miniforge/miniforge.sh
@@ -5,10 +5,16 @@ set -xeuo pipefail
 wget -q -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3.sh -b -p "/conda"
 . /bin/conda-activate.sh
+
+# upgrade all packages
 mamba upgrade --all --yes
 
 # merging in what we want (not removing existing packages)
 mamba env update --file environment.yml
+
+# this should end up in '/usr/local/...' for jupyter
+jupyter kernelspec remove python3
+python3 -m ipykernel install --name miniconda2023 --display-name "Miniconda 2023 (Anaconda)"
 
 # cleanup
 mamba clean --all --yes
