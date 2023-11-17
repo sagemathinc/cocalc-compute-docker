f209baa286c6e2531b2249f2fcfb82f8b022479c
Harald Schilly <harald.schilly@gmail.com>
2023-11-17 15:51:33 +0100

    colab: force remove kernel

--- src/colab/colab.sh
+++ src/colab/colab.sh
@@ -63,7 +63,7 @@ pip --no-cache-dir install -r pip.txt
 
 # pip.txt should contain an ipykernel, we just ensure it is here, no updating!
 pip install ipykernel
-jupyter kernelspec remove -y python3
+jupyter kernelspec remove -f -y python3
 python3 -m ipykernel install --sys-prefix --name python3 --display-name "Python 3 (Colab)"
 
 pip cache purge
