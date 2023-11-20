e0ad88de48db20fbfc0775a27af2af565737d8cb
William Stein <wstein@sagemath.com>
2023-11-20 01:34:03 +0000

    fix online disk resizing when disk is full

--- src/cocalc/disk_enlarger.py
+++ src/cocalc/disk_enlarger.py
@@ -35,8 +35,19 @@ for line in iter(process.stdout.readline, b''):
     for dev in [b'sda', b'nvme0n1']:
         if line.find(b'%s: detected capacity change' % dev) != -1:
             print("%s disk device changed size -- growing partition" % dev)
+            run([
+                'mount', '-o', 'size=10M,rw,nodev,nosuid', '-t', 'tmpfs',
+                'tmpfs', '/tmp'
+            ])
             run(['growpart', '/dev/%s' % dev.decode(), '1'])
             result = subprocess.run(['df', '/'], stdout=subprocess.PIPE)
             output = result.stdout.decode()
             root_device = output.split('\n')[1].split()[0]
             run(['resize2fs', root_device])
+            run(['umount', '/tmp'])
+
+# NOTE -- we mount a ramdisk on /tmp because if the disk is full then writing to /tmp is
+# not possible, and growpart writes to /tmp!  At least RAM is not full, so this is a workaround.
+# See https://stackoverflow.com/questions/59420015/unable-to-growpart-because-no-space-left
+# This is a VERY likely edge case to get into, since you only think to enlarge your disk
+# when you run out.
