0a46bd4dc57aa124a089ea50b684d8188c65d21e
William Stein <wstein@sagemath.com>
2023-11-23 00:40:44 +0000

    run cron if it is installed

--- src/cocalc/start-compute.sh
+++ src/cocalc/start-compute.sh
@@ -10,5 +10,8 @@ source /cocalc/start-env.sh
 sudo hostname `cat /cocalc/conf/hostname`
 echo "127.0.0.1 `cat /cocalc/conf/hostname`" | sudo tee -a /etc/hosts
 
+# If cron is installed, start it.
+sudo service cron start || true
+
 cd /cocalc/src/compute/compute
 node ./start-compute.js
