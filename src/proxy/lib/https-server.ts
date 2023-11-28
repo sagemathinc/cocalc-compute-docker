1748d2ac2e9e4dbe36655e3bea972c4df2438f87
William Stein <wstein@sagemath.com>
2023-11-28 06:43:53 +0000

    proxy server: make it listen

--- src/proxy/lib/https-server.ts
+++ src/proxy/lib/https-server.ts
@@ -3,12 +3,25 @@
 import express from "express";
 import { createServer } from "https";
 import genCert from "./gen-cert";
+import debug from "debug";
+import { callback } from "awaiting";
 
-export default async function httpsServer() {
+const log = debug("http-server");
+
+export default async function httpsServer({
+  port = 443,
+  host = "0.0.0.0",
+}: {
+  port: number;
+  host: string;
+}) {
+  log("creating https server", { port, host });
   const app = express();
   const cert = await genCert();
-  console.log({ cert, app });
   const server = createServer(cert, app);
+
+  log(`starting proxy server listening on ${host}:${port}`);
+  await callback(server.listen.bind(server), port, host);
 
   return server;
 }
