b814d0b8dabfa73473ac83c2cf262bf2824e1d86
William Stein <wstein@sagemath.com>
2024-03-10 18:59:07 +0000

    automatically configure two default conda channels if none are configured
    - doing this in ~/.condarc, so that it is persistent
    - only does this if ~/.condarc is not already there, since we want to
      respect any existing user choices

--- src/anaconda/bash.sh
+++ src/anaconda/bash.sh
@@ -4,3 +4,8 @@
 export MAMBA_ROOT_PREFIX=/conda
 eval "$(micromamba shell hook --shell bash)"
 micromamba activate default
+if [ ! -f /home/user/.condarc ]; then
+    # Add two default channels
+    micromamba config append channels anaconda
+    micromamba config append channels conda-forge
+fi
