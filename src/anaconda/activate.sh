331e1cf55e8f56ba52d2545c7fabc67b8e531500
William Stein <wstein@sagemath.com>
2024-03-11 00:46:28 +0000

    switch -- micromamba --> miniforge3; basically miniforge3 works VASTLY better in all my tests, so I'm going with it.  It's recommended these days, etc.

---
+++ src/anaconda/activate.sh
@@ -0,0 +1,5 @@
+# Activate default environment.
+
+eval "$(/conda/bin/conda shell.bash hook)"
+
+
--- src/anaconda/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-eval "$(micromamba shell hook --shell bash)"
-micromamba activate default
-micromamba install $*
