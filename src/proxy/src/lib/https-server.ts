3f1065cc8b9712ddf8dd7c162fb841b5c77e4b98
William Stein <wstein@sagemath.com>
2024-03-15 12:47:41 +0000

    proxy: work in progress refactoring code so can do a full restart on config change

--- src/proxy/src/lib/https-server.ts
+++ src/proxy/src/lib/https-server.ts
@@ -1,13 +1,8 @@
 /* Simple authenticated https server */
 
-import express, { Router, urlencoded } from "express";
-import cookieParser from "cookie-parser";
-import { createServer } from "https";
 import genCert from "./gen-cert";
 import debug from "debug";
-import { callback } from "awaiting";
 import enableProxy from "./proxy";
-import enableAuth, { AUTH_PATH } from "./auth";
 
 const log = debug("proxy:http-server");
 
@@ -35,42 +30,10 @@ export default async function httpsServer({
     authTokenPath = process.env["PROXY_AUTH_TOKEN_FILE"];
   }
 
-  const app = express();
-  const router = Router();
+  // TODO: support letsencrypt option
   const cert = await genCert();
-  const server = await createServer(cert, app);
+  // TODO: setup a redirect to port 443
   //const server = require('http').createServer(app);
 
-  if (authTokenPath) {
-    log(
-      `enabling auth token for CoCalc proxy server -- path = '${authTokenPath}'`,
-    );
-
-    // Enable URL-encoded bodies, but ONLY for AUTH_PATH (!),
-    // since otherwise this mangles the proxying of the target
-    // sites, which is very, very bad in some cass, e.g.,
-    // JupyterHub sign in is broken.
-    router.use(
-      AUTH_PATH,
-      // type argument is just to minimize the impact
-      urlencoded({ extended: true, type: "application/x-www-form-urlencoded" }),
-    );
-    // Use the cookie-parser middleware so req.cookies is defined.
-    app.use(cookieParser());
-    await enableAuth({ router, authTokenPath });
-  } else {
-    log("auth token not enabled -- any client can connect to the proxied site");
-  }
-  enableProxy({ router, server, configPath });
-
-  app.use(router);
-
-  log(`starting CoCalc proxy server listening on ${host}:${port}`);
-  await callback(server.listen.bind(server), port, host);
-
-  app.on("error", (err) => {
-    log("app ERROR", err);
-  });
-
-  return server;
+  enableProxy({ cert, configPath, authTokenPath, port, host });
 }
--- src/proxy/src/lib/proxy.ts
+++ src/proxy/src/lib/proxy.ts
@@ -2,6 +2,12 @@ import debug from "debug";
 import { createProxyServer } from "http-proxy";
 import { readFile } from "fs/promises";
 import { watch } from "chokidar";
+import enableAuth, { AUTH_PATH } from "./auth";
+import { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import express, { Router } from "express";
+import { createServer } from "https";
+import { callback } from "awaiting";
 
 const log = debug("proxy:create-proxy");
 
@@ -16,26 +22,72 @@ const ChokidarOpts = {
 } as const;
 
 export default async function createProxy({
-  server,
-  router,
+  cert,
   configPath,
+  authTokenPath,
+  port,
+  host,
 }: {
-  router;
-  server;
+  cert;
   configPath: string;
+  authTokenPath?: string;
+  port: number;
+  host: string;
 }) {
-  log("creating proxy server");
+  log("creating proxy server with config from ", configPath);
+  const app = express();
+  app.on("error", (err) => {
+    log("app ERROR", err);
+  });
+
+  const server = await createServer(cert, app);
+  const router = Router();
 
   let disable: null | Function = null;
+  let config: null | Configuration = null;
   const updateConfigPath = async () => {
     log("updating configuration from ", configPath);
     if (disable != null) {
       disable();
     }
-    const config: Configuration = JSON.parse(
+    const newConfig: Configuration = JSON.parse(
       (await readFile(configPath)).toString().trim(),
     );
-    log("loaded config", config);
+    log("loaded config from ", configPath, " -- ", newConfig);
+    // weak equality check is fine for this
+    if (JSON.stringify(config) == JSON.stringify(newConfig)) {
+      log("no config change");
+      return;
+    }
+    log("config changed");
+    config = newConfig;
+
+    if (authTokenPath) {
+      log(
+        `enabling auth token for CoCalc proxy server -- path = '${authTokenPath}'`,
+      );
+
+      // Enable URL-encoded bodies, but ONLY for AUTH_PATH (!),
+      // since otherwise this mangles the proxying of the target
+      // sites, which is very, very bad in some cass, e.g.,
+      // JupyterHub sign in is broken.
+      router.use(
+        AUTH_PATH,
+        // type argument is just to minimize the impact
+        urlencoded({
+          extended: true,
+          type: "application/x-www-form-urlencoded",
+        }),
+      );
+      // Use the cookie-parser middleware so req.cookies is defined.
+      app.use(cookieParser());
+      await enableAuth({ router, authTokenPath });
+    } else {
+      log(
+        "auth token not enabled -- any client can connect to the proxied site",
+      );
+    }
+
     disable = createRoutes(server, router, config);
   };
 
@@ -46,7 +98,10 @@ export default async function createProxy({
   });
   await updateConfigPath();
 
-  return router;
+  app.use(router);
+
+  log(`starting CoCalc proxy server listening on ${host}:${port}`);
+  await callback(server.listen.bind(server), port, host);
 }
 
 function createRoutes(server, router, config) {
