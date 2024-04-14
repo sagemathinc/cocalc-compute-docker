a95859b924fea7e93a5055110736c074ecfaed6d
William Stein <wstein@sagemath.com>
2024-04-14 18:16:44 +0000

    make graphics in R Studio work by default

--- src/rstats/build-r.sh
+++ src/rstats/build-r.sh
@@ -12,12 +12,21 @@ curl -O https://cran.rstudio.com/src/base/R-4/R-${R_VERSION}.tar.gz
 tar -xzvf R-${R_VERSION}.tar.gz
 cd R-${R_VERSION}
 
+# configure is inspired by https://discourse.openondemand.org/t/graphics-display-issues-with-r-built-from-source-for-rstudio-server/1123/5
+# Also, we need this x, cairo, etc. support so graphics work in R Studio (see https://github.com/sagemathinc/cocalc-compute-docker/issues/9).
+
 ./configure \
     --prefix=/usr/local \
     --enable-R-shlib \
+    --enable-R-profiling \
     --enable-memory-profiling \
     --with-blas \
-    --with-lapack
+    --with-lapack \
+    --with-x \
+    --with-cairo \
+    --with-jpeglib \
+    --with-readline \
+    --with-tcltk
 
 make -j4
 
