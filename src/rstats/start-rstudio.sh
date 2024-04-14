14792b10b64c05d8c3c8ab7c142d0ea338c49a31
William Stein <wstein@sagemath.com>
2024-04-14 15:40:29 +0000

    fix #9 -- rstudio version conflict

--- src/rstats/start-rstudio.sh
+++ src/rstats/start-rstudio.sh
@@ -2,6 +2,11 @@
 
 set -ev
 
+# RSTUDIO_WHICH_R
+#  - https://github.com/sagemathinc/cocalc-compute-docker/issues/9
+#  - https://forum.posit.co/t/use-a-different-r-version-temporarily-in-rstudio/20848/7
+export RSTUDIO_WHICH_R=/usr/local/bin/R
+
 echo "user:$(sudo cat /cocalc/conf/auth_token)" | sudo chpasswd
 sudo chown user:user -R /var/run/rstudio* /var/lib/rstudio-server/
 sudo sh -c "echo 'server-user=user' > /etc/rstudio/server.conf"
