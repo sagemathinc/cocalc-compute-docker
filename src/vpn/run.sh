cd41ca5f05a2fd5fee11ef5ce3bcf195699f0328
William Stein <wstein@sagemath.com>
2024-05-28 15:39:08 +0000

    fixing vpn issues -- more work has to be done to update wg interface in place

--- src/vpn/run.sh
+++ src/vpn/run.sh
@@ -13,10 +13,9 @@ python3 /conf.py
 cp -v hosts /cocalc/conf/hosts
 
 # also if pings.sh was written, copy it over as well, so the host
-# can run it
+# can run it -- these take a while so we can't run them here.
 if [ -e pings.sh ]; then
    cp -v pings.sh /cocalc/conf
-   chmod a+x /cocalc/conf/pings.sh
 else
    # ensure it is not there if not needed
    rm -f /cocalc/conf/pings.sh
@@ -25,7 +24,13 @@ fi
 # actually do the configuration -- if up doesn't work, e.g., because
 # the interface already exists, we strip and update it as explained
 # in the wg-quick manpage.
-id=`cat /cocalc/conf/compute_server_id`
-if ! wg-quick up ./wg${id}.conf; then
-    wg syncconf wg$id <(wg-quick strip ./wg$id.conf)
+#   https://www.reddit.com/r/WireGuard/comments/fodgpi/adding_peer_without_having_to_restart_service/
+if ! wg-quick up ./wg.conf; then
+    wg syncconf wg <(wg-quick strip ./wg.conf)
+    if [ -f ./add-routes.sh ]; then
+        # We MUST add the following (ignoring errors) for every other node
+        # since syncconf doesn't do this:
+        #    ip -4 route add 10.11.200.100/32 dev wg || true
+        ./add-routes.sh
+    fi
 fi
