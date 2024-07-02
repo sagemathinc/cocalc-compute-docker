56e4d11adfe855fe53954c5b971e818c696eb0b4
William Stein <wstein@sagemath.com>
2024-07-02 17:43:15 +0000

    make proxy server work even if bash != sh

--- src/rstats/start-rstudio.sh
+++ src/rstats/start-rstudio.sh
@@ -9,7 +9,7 @@ export RSTUDIO_WHICH_R=/usr/local/bin/R
 
 echo "user:$(sudo cat /cocalc/conf/auth_token)" | sudo chpasswd
 sudo chown user:user -R /var/run/rstudio* /var/lib/rstudio-server/
-sudo sh -c "echo 'server-user=user' > /etc/rstudio/server.conf"
+sudo bash -c "echo 'server-user=user' > /etc/rstudio/server.conf"
 # Yes, this is quite a hack!
 sudo sed -i '/rstudio-server/c\rstudio-server:x:2001:2001::/home/user:/bin/bash' /etc/passwd
 
