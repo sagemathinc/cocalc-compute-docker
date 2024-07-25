e555a928e726d3c84dcd2c4f45190ccef8e3a3de
William Stein <wstein@sagemath.com>
2024-07-25 19:33:31 +0000

    workaround https://github.com/sagemath/sage/pull/37856

--- src/sagemath/scripts/links-to-sage.sh
+++ src/sagemath/scripts/links-to-sage.sh
@@ -6,4 +6,4 @@ ln -sf "/usr/local/sage/sage" /usr/bin/sage
 ln -sf "/usr/local/sage/sage" /usr/bin/sagemath
 
 # Put scripts to start gap, gp, maxima, ... in /usr/local/bin
-/usr/local/sage/sage --nodotsage -c "install_scripts('/usr/local/bin')"
+/usr/local/sage/sage -c "import sys; sys.path.append('.'); from install_scripts import install_scripts; install_scripts('/usr/local/bin')"
