0807a771cb99340f5ef10b8d90ccb29d6a7139bd
William Stein <wstein@sagemath.com>
2024-09-17 18:49:48 +0000

    fix #16 -- use konsole instead of xterm as the default application

--- src/cocalc/apps/xpra.sh
+++ src/cocalc/apps/xpra.sh
@@ -9,9 +9,8 @@ which xpra
 if [ $? -ne 0 ]; then
     echo "Installing xpra"
     sudo apt-get update
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xpra xterm
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xpra xterm konsole
 fi
 
 set -ev
-xpra start --bind-tcp=localhost:$PORT --sharing=yes  --daemon=no --start=xterm
-
+xpra start --bind-tcp=localhost:$PORT --sharing=yes  --daemon=no --start='/etc/X11/Xsession true' --start='/usr/bin/konsole'
