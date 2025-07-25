d32e374d3f6f7e40c0e8cf2e1b83c82fc3bb6e75
William Stein <wstein@sagemath.com>
2025-07-25 00:01:09 +0000

    newer python/anaconda

--- src/python/install-core-python.sh
+++ src/python/install-core-python.sh
@@ -6,10 +6,10 @@ set -ev
 apt-get update
 apt-get install -y python3-pip python3-dev
 
-pip install jupyter
+pip install --break-system-packages jupyter
 python3 -m ipykernel install --name python3 --display-name "Python `python3 --version | awk '{print $2}'`"
 
 # Install the very popular python libraries, which are pretty
 # safe to install in any python environment.
-pip install matplotlib pandas numpy scipy scikit-learn requests beautifulsoup4 sympy mpmath pyyaml networkx
+pip install --break-system-packages  matplotlib pandas numpy scipy scikit-learn requests beautifulsoup4 sympy mpmath pyyaml networkx
 
